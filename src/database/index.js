bab968520c1d17d6916d856be7eb4021dc0a71a0
Luciano J Guerra <dsibr@hotmail.com>
2020-01-17 14:11:54 -0300

    ADD - Relation User model with File model. Store file name into files datatable

--- src/database/index.js
+++ src/database/index.js
@@ -2,8 +2,9 @@ import Sequelize from 'sequelize';
 import databaseConfig from '../config/database';
 
 import User from '../app/models/User/User';
+import File from '../app/models/File/File';
 
-const models = [User];
+const models = [User, File];
 
 class Database {
   constructor() {
@@ -12,7 +13,9 @@ class Database {
 
   init() {
     this.connection = new Sequelize(databaseConfig);
-    models.map(model => model.init(this.connection));
+    models
+      .map(model => model.init(this.connection))
+      .map(model => model.associate && model.associate(this.connection.models));
   }
 }
 export default new Database();
--- src/routes.js
+++ src/routes.js
@@ -3,15 +3,23 @@ import { Router } from 'express';
 import multer from 'multer';
 import multerConfigs from './config/multer';
 
+/**
+ * Controller
+ */
 import UserController from './app/controllers/User/UserController';
 import SessionController from './app/controllers/Session/SessionController';
+import FileController from './app/controllers/File/FileController';
+
+/**
+ * Middleware
+ */
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 const upload = multer(multerConfigs);
 
 routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello word' });
+  return res.json({ message: 'goBarberApi' });
 });
 
 /**
@@ -27,8 +35,6 @@ routes.use(authMiddleware);
 routes.put('/users', authMiddleware, UserController.update);
 
 // upload test
-routes.post('/files', upload.single('file'), (req, res) => {
-  return res.json({ ok: true });
-});
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
