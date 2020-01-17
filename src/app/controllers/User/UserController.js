3d017faff22a6df1dff682c6946c94d1b52a69c4
Luciano J Guerra <dsibr@hotmail.com>
2020-01-17 12:46:59 -0300

    ADD - Validate in SessionController and UserController update and store methods

--- src/app/controllers/User/UserController.js
+++ src/app/controllers/User/UserController.js
@@ -1,7 +1,25 @@
+import * as Yup from 'yup';
 import User from '../../models/User/User';
 
 class UserController {
+  /**
+   * Store user
+   */
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string()
+        .required()
+        .min(6),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Preencha os campos corretamente' });
+    }
+
     const checkUser = await User.findOne({ where: { email: req.body.email } });
     if (checkUser) {
       return res.status(400).json({ error: 'E-mail já cadastrado' });
@@ -10,7 +28,28 @@ class UserController {
     return res.json({ id, name, email, provider });
   }
 
+  /**
+   * Update user
+   */
   async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      oldPassword: Yup.string().min(6),
+      password: Yup.string()
+        .min(6)
+        .when('oldPassword', (oldPassword, field) =>
+          oldPassword ? field.required() : field
+        ),
+      passwordConfirm: Yup.string().when('password', (password, field) =>
+        password ? field.required().oneOf([Yup.ref('password')]) : field
+      ),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Preencha os campos corretamente' });
+    }
+
     const user = await User.findByPk(req.userId);
     const { name, oldPassword } = req.body;
 
