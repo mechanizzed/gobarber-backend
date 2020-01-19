5434fc34f15bfbf75cea2237852def1adcd56751
Luciano J Guerra <dsibr@hotmail.com>
2020-01-19 12:45:25 -0300

    ADD - Errors exceptions message and Sentry setup for project

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
-import path from 'path';
 import express from 'express';
+import path from 'path';
+import Youch from 'youch';
+import * as Sentry from '@sentry/node';
+import 'express-async-errors';
 import routes from './routes';
+import sentryConfig from './config/sentry';
 
 import './database';
 
@@ -8,11 +12,18 @@ class App {
   constructor() {
     this.server = express();
 
+    Sentry.init({
+      dsn: 'https://687ea55ad24545a89781a4d84c8fc68c@sentry.io/1887391',
+    });
+
     this.middlewares();
     this.routes();
+
+    this.exceptionHandler();
   }
 
   middlewares() {
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
     this.server.use(
       '/files',
@@ -22,6 +33,14 @@ class App {
 
   routes() {
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      const errors = await new Youch(err, req).toJSON();
+      return res.status(500).json(errors);
+    });
   }
 }
 
---
+++ src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+  dns: 'https://687ea55ad24545a89781a4d84c8fc68c@sentry.io/1887391',
+};
